53a0cf2ad1a158cc271b972e772cb01b36558e83
justin.hobbs <justinhobbs@fastmail.com>
2022-05-17 12:22:31 -0500

    commit tree f4bf49c6bbfd93aef95e42e15e688d95f2ff77ed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun()
+saturdayFun("bathe my dog")
+
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(result = "") {
+    const innerFunction = function (empathetic = "a hard worker") {
+        return `You are ${result}${empathetic}${result}!`
+    };
+    return innerFunction
+}
